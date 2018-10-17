4ec2260781cdcd567994ffbd0262853bb49a7eca
Succulent <16302010034@fudan.edu.cn>
2018-10-17 21:33:23 +0800

    modify test.py to adjust own data set

--- cnn.py
+++ cnn.py
@@ -25,23 +25,29 @@ biases_fc1 = tf.Variable(tf.constant(0.1, shape=[1024]))
 res_pool2_reshape = tf.reshape(res_pool2, [-1, 7*7*64])
 res_fc1 = tf.nn.relu(tf.matmul(res_pool2_reshape, weights_fc1) + biases_fc1)
 
+keep_prob = tf.placeholder('float')
+res_fc1_drop = tf.nn.dropout(res_fc1, keep_prob)
+
 weights_fc2 = tf.Variable(tf.truncated_normal([1024, 14], stddev=0.1))
 biases_fc2 = tf.Variable(tf.constant(0.1, shape=[14]))
 
-outputs = tf.nn.softmax(tf.matmul(res_fc1, weights_fc2) + biases_fc2)
+outputs = tf.nn.softmax(tf.matmul(res_fc1_drop, weights_fc2) + biases_fc2)
 
 cross_entropy = -tf.reduce_sum(expec_outputs*tf.log(outputs))
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-correct_pre = tf.equal(tf.arg_max(expec_outputs, 1), tf.arg_max(outputs, 1))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_pre = tf.equal(tf.argmax(outputs, 1), tf.argmax(expec_outputs, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_pre, 'float'))
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 data = img_data.Data()
-for i in range(100):
+
+for i in range(1000):
     batch = data.train_set(50)
-    test = data.test_set(20)
-    train_step.run(feed_dict={img_inputs: batch[0], expec_outputs: batch[1]}, session=sess)
-    train_acc = accuracy.eval(feed_dict={img_inputs: test[0], expec_outputs: test[1]}, session=sess)
+    test = data.test_set(200)
+    train_step.run(feed_dict={img_inputs: batch[0], 
+                    expec_outputs: batch[1], keep_prob: 0.5}, session=sess)
+    train_acc = accuracy.eval(feed_dict={img_inputs: test[0], 
+                    expec_outputs: test[1], keep_prob: 1.0}, session=sess)
     print("train time: %d, accuracy: %f" % (i, train_acc))
