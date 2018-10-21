050dfdf4068a2d46407ec7c602c33cd4fa78efbe
刘欣彪 <liuxinbiao@liuxinbiaodeMacBook-Pro.local>
2018-10-21 14:39:15 +0800

    modify v2

--- cvv_v6.py
+++ cvv_v6.py
@@ -1,9 +1,8 @@
-# inputs->conv->bn->relu->pool->fc->relu->fc->softmax
-# train time: 5000, accuracy: 91%， run time: 562s
+# inputs -> (conv -> relu -> pool)x2 -> fc -> relu -> fc -> softmax
+# accuracy: 99.8%-100%
 
 import img_data
 import tensorflow as tf
-import  numpy as np
 import time
 from tensorflow.python.training.moving_averages import assign_moving_average
 
@@ -51,16 +50,24 @@ img_reshape = tf.reshape(img_inputs, [-1, 28, 28, 1])
 res_conv1 = tf.nn.conv2d(img_reshape, weights_conv1, strides=[1, 1, 1, 1], padding='SAME') + biases_conv1
 
 train = tf.placeholder(tf.bool)
-res_batch = batch_norm(res_conv1, train)
-res_relu1 = tf.nn.relu(res_batch)
-res_pool1 = tf.nn.max_pool(res_relu1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-weights_fc1 = tf.Variable(tf.truncated_normal([14*14*32, 1024], stddev=0.1))
+res_batch1 = batch_norm(res_conv1, train)
+res_lrelu1 = tf.nn.leaky_relu(res_batch1)
+res_pool1 = tf.nn.max_pool(res_lrelu1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+weights_conv2 = tf.Variable(tf.truncated_normal([5, 5, 32, 64], stddev=0.1))
+biases_conv2 = tf.Variable(tf.constant(0.1, shape=[64]))
+
+res_conv2 = tf.nn.conv2d(res_pool1, weights_conv2, strides=[1, 1, 1, 1], padding='SAME') + biases_conv2
+res_batch2 = batch_norm(res_conv2, )
+res_pool2 = tf.nn.max_pool(res_conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+weights_fc1 = tf.Variable(tf.truncated_normal([7*7*64, 1024], stddev=0.1))
 biases_fc1 = tf.Variable(tf.constant(0.1, shape=[1024]))
 
-res_pool1_reshape = tf.reshape(res_pool1, [-1, 14*14*32])
+res_pool2_reshape = tf.reshape(res_pool2, [-1, 7*7*64])
 
-res_fc1 = tf.nn.relu(tf.matmul(res_pool1_reshape, weights_fc1) + biases_fc1)
+res_fc1 = tf.nn.relu(tf.matmul(res_pool2_reshape, weights_fc1) + biases_fc1)
 
 weights_fc2 = tf.Variable(tf.truncated_normal([1024, 14], stddev=0.1))
 biases_fc2 = tf.Variable(tf.constant(0.1, shape=[14]))
@@ -75,6 +82,7 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 data = img_data.Data()
 
 sess.run(tf.global_variables_initializer())
+
 time_start = time.time()
 for i in range(5000):
     batch = data.train_set(50)
@@ -83,7 +91,7 @@ for i in range(5000):
         train_accuracy = accuracy.eval(feed_dict={img_inputs: test[0],
                         expect_outputs: test[1]}, session=sess)
         print("step %d, training accuracy %g%%" % (i+1, train_accuracy*100))
-    train_step.run(feed_dict={img_inputs: batch[0], expect_outputs: batch[1], train: True}, session=sess)
+    train_step.run(feed_dict={img_inputs: batch[0], expect_outputs: batch[1]}, session=sess)
 time_end = time.time()
 test = data.test_set(1000)
 final_accuracy = accuracy.eval(feed_dict={img_inputs: test[0], expect_outputs: test[1]}, session=sess)
